116fef9bc03e6a03e096ad6b9ff04dfd759433fb
cassiesancartier <cassie.sancartier@gmail.com>
2013-07-14 15:00:50 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,64 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: Cassie Sancartier
+
+# 2.1: Based on the linked article, it seems that because of the leading zero, 
+# python is translating the 02132 number from the Octal system into another type 
+# of number system. There was an 'invalid token' error message on the first number 
+# (02492) because the Octal system only uses the digits 0 through 7.
+
+-------------------------
+
+# 2.2: When the original statements are run in a script, there is no output.  
+# Here is the modified script, with print statement:
+
+print 5
+x = 5
+print x + 1
+
+# The new output is:
+5
+6
+
+-------------------------
+
+# 2.3: Value and type of expression:
+# 1. 8, integer
+# 2. 8.5, float
+# 3. 4.0, float
+# 4. 11, integer
+# 5. '.....', string
+
+-------------------------
+
+# 2.4:
+
+# 1. 523.6 (used the following expression, with floats instead of integers):
+
+r = 5
+4.0/3.0 * math.pi * r**3
+523.5987755982989
+
+#2. $945.45 (used the following):
+
+discount_price = 24.95 * .60
+shipping_first_copy = 3.00
+shipping_additional_copy = .75
+
+(discount_price * 60) + (shipping_first_copy + (shipping_additional_copy * 59))
+945.4499999999999
+
+#3. 7:30:02am  (used the following):
+
+easy_pace = ((8 * 60) + 15)
+tempo_pace = ((7 * 60) + 12)
+start_time = ((6 * 3600) + (52 * 60))
+start_time + (easy_pace * 2.0) + (tempo_pace * 3.0)
+27006.0
+breakfast_time = start_time + (easy_pace * 2.0) + (tempo_pace * 3.0)
+
+breakfast_time/3600
+7.501666666666667
+
+
+
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,76 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: Cassie Sancartier
+
+# 3.1: When you move the repeat_lyrics call to the top of the program,
+# you get the following error, because you are trying to call a function you
+# haven't yet defined:
+
+CassieSancartier:RampUp cms$ python exercise3.1.py
+Traceback (most recent call last):
+  File "exercise3.1.py", line 1, in <module>
+    repeat_lyrics()
+NameError: name 'repeat_lyrics' is not defined
+
+-------------------------
+
+# 3.2: When you move the function call back to the bottom, but then put the 
+# repeat_lyrics definition above the print_lyrics definition, you get the 
+# following:
+
+"I'm a lumberjack, and I'm okay"
+"I sleep all night and I work all day"
+"I'm a lumberjack, and I'm okay"
+"I sleep all night and I work all day"
+
+# The repeat_lyrics() call works as it should because both the print_lyrics()
+# and repeat_lyrics() functions were defined before calling it - it doesn't
+# matter that they are 'out of order'.
+
+-------------------------
+
+# 3.3: Here is a function that moves a string's last letter to column 70:
+
+def right_justify(s):
+	spaces = (70 - len(s))
+	print ((" " * spaces) + s)
+
+	
+right_justify('boat')
+                                                                  boat
+
+-------------------------
+
+# 3.4:
+
+# 1. Tested it, returned 'spam' twice
+
+# 2. Modified do_twice:
+
+def do_twice(f, v):
+	f(v)
+	f(v)
+
+# 3. print_twice function:
+
+def print_twice(string):
+	print string
+	print string
+
+# 4. Called modified version of do_twice:
+
+do_twice(print_twice, 'spam')
+spam
+spam
+spam
+spam
+
+# 5. New function, do_four:
+
+def do_four(f, v):
+	do_twice(f, v)
+	do_twice(f, v)
+
+
+			
+
